f906d0311b8ed87a6dd178f5d07afd5d94e62d34
Frans Alexander <dev.norberg@gmail.com>
2022-06-13 15:56:30 +0200

    Finish Rock Paper Scissor IF ELSE conditions

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
-const playerSelection = "rock"
+const playerSelection = "paper"
 const computerSelection = "paper"
 
 function playRound(playerSelection, computerSelection) {
 
-
+    //rock choice
     if (playerSelection == "rock" && computerSelection == "scissor") {
         return `You Win ${playerSelection} Beats ${computerSelection}.`
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         return `You Loose ${computerSelection} Beats ${playerSelection}.`
+        
+    //Paper choice
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return `You Win ${playerSelection} Beats ${computerSelection}.`
+    } else if (playerSelection == "paper" && computerSelection == "scissor") {
+        return `You Loose ${computerSelection} Beats ${playerSelection}.`
+
+    //Scissor Choice
+    } else if (playerSelection == "scissor" && computerSelection == "paper") {
+        return `You Win ${playerSelection} Beats ${computerSelection}.`
+    } else if (playerSelection == "scissor" && computerSelection == "rock") {
+        return `You Loose ${computerSelection} Beats ${playerSelection}.`
+    //same Choice
+    } else if (playerSelection == computerSelection){
+        return "DRAW go again"
     }
 
 }
